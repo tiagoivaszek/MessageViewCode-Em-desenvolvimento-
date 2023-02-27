54f0b7b75629c855b18cd63e86efc2ef2fb11d50
Tiago Ivaszek Jensen <tiagoivaszek@hotmail.com>
2023-02-27 15:36:26 -0300

    Ajustes

--- WhatsAppViewCode/Home/ViewController/HomeViewController.swift
+++ WhatsAppViewCode/Home/ViewController/HomeViewController.swift
@@ -6,17 +6,33 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseFirestore
 
 class HomeViewController: UIViewController {
+
+    
+
+    
     
     var screen: HomeScreen!
+    var auth: Auth!
+    var firestore: Firestore!
+    
+    var idUsuarioLogado: String?
+    var emailUsuarioLogadp: String?
+    var alert: Alert!
+    var screenContact: Bool?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.navigationController?.navigationBar.isHidden = true
         self.view.backgroundColor = CustomColor.appLight
-      
+        self.configHomeView()
+        self.configCollectionView()
+              
     }
     
     override func loadView() {
@@ -26,6 +42,47 @@ class HomeViewController: UIViewController {
         
     }
     
+    private func configHomeView(){
+        self.screen.navView.delegate(delegate: self)
+    }
+    
+    private func configCollectionView(){
+        self.screen.delegateCollectionView(delegate: self, dataSource: self)
+    }
+    
+    
+}
 
+extension HomeViewController: navViewProtocol{
+    
+    func typeScreenMessagem(type: TypeConversationOrContact) {
+        
+        switch type {
+        case .contact:
+            self.screenContact = true
+        case .conversation:
+            self.screenContact = false
+        }
+        
+    }
+    
+}
 
+extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(indexPath)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: 100)
+    }
 }
